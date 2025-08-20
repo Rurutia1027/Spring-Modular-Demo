194121507033f1bbb78fe2b36ca6023ca8fd1f0a
Rurutia1027 <rurutia1027@gmail.com>
2025-08-20 13:22:19 +0800

    fix: remove previous annotation

--- Spring-Project/project/src/main/java/org/medium/demo/project/ProjectSpringApplication.java
+++ Spring-Project/project/src/main/java/org/medium/demo/project/ProjectSpringApplication.java
@@ -23,8 +23,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("org.medium.demo.project.entity")
-@EnableJpaRepositories("org.medium.demo.project.repository")
 public class ProjectSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectSpringApplication.class, args);
